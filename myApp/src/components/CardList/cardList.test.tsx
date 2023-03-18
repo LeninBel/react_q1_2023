7684503d877bb4ec8f04aaec7467c11a824a9719
lenin_by <lenapatuk@Lenas-MacBook-Pro.local>
2023-03-18 09:43:38 +0100

    feat: cardList tests

---
+++ myApp/src/components/CardList/cardList.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import CardList from './cardList';
+
+const books = [
+  {
+    title: 'TestBook',
+    image: 'TestImage',
+    category: 'TestCategory',
+    author: 'TestAuthor',
+  },
+  {
+    title: 'TestBook1',
+    image: 'TestImage1',
+    category: 'TestCategory1',
+    author: 'TestAuthor1',
+  },
+];
+
+describe('CardList', () => {
+  beforeEach(() => {
+    render(<CardList books={books} />);
+  });
+
+  it('renders all books category', () => {
+    screen
+      .getAllByTestId('category')
+      .forEach((c, i) => expect(c).toHaveTextContent(books[i].category));
+  });
+
+  it('renders all books title', () => {
+    screen.getAllByTestId('title').forEach((c, i) => expect(c).toHaveTextContent(books[i].title));
+  });
+
+  it('renders all books author', () => {
+    screen.getAllByTestId('author').forEach((c, i) => expect(c).toHaveTextContent(books[i].author));
+  });
+});
