4d94e48b34a69265ece69407dc9392bbd5d07b47
lenin_by <lenapatuk@Lenas-MacBook-Pro.local>
2023-03-22 15:57:30 +0100

    feat: add toast message

--- myApp/src/pages/Form/FormPage.tsx
+++ myApp/src/pages/Form/FormPage.tsx
@@ -3,30 +3,36 @@ import Header from '../../components/Header/header';
 import FormContainer from '../../components/Form/FormContainer/formContainer';
 import { Book } from '../../data/mockedData';
 import CardList from '../../components/CardList/cardList';
+import Toast from '../../components/Toast/toast';
 
 interface IState {
   books: Array<Book>;
+  newAdded: boolean;
 }
 
 class FormPage extends React.Component<Record<number, unknown>, IState> {
   constructor(props: Record<number, unknown>) {
     super(props);
-    this.state = { books: [] };
+    this.state = { books: [], newAdded: false };
     this.addBook = this.addBook.bind(this);
   }
 
   addBook(book: Book) {
-    this.setState((prev) => ({ books: [...prev.books, book] }));
+    this.setState((prev) => ({ books: [...prev.books, book], newAdded: true }));
+    setTimeout(() => {
+      this.setState((prev) => ({ books: prev.books, newAdded: false }));
+    }, 3000);
   }
 
   render() {
-    const { books } = this.state;
+    const { books, newAdded } = this.state;
     return (
-      <>
+      <div style={{ position: 'relative' }}>
+        <Toast show={newAdded} />
         <Header title="Form" />
         <FormContainer addBook={this.addBook} />
         <CardList books={books} searchTerm="" />
-      </>
+      </div>
     );
   }
 }
